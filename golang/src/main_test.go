5f91e253d3d3ac2fac156b68e92cee47dad886ea
sunakan <sunakan03@gmail.com>
2019-10-19 18:04:07 +0900

    [Add] goで環境変数をGETする

--- golang/src/main_test.go
+++ golang/src/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
@@ -8,5 +9,20 @@ func TestHello(t *testing.T) {
 	result := Hello()
 	if result != "Hello" {
 		t.Fatal("テスト失敗")
+	}
+}
+
+func TestGetEnv(t *testing.T) {
+	notExisted         := os.Getenv("NOT_EXISTED")
+	awsAccessKeyId     := os.Getenv("AWS_ACCESS_KEY_ID")
+	awsSecretAccessKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
+	if notExisted != "" {
+		t.Fatal("環境変数NOT_EXISTEDは空文字であるべき")
+	}
+	if awsAccessKeyId != "dummy" {
+		t.Fatal("環境変数AWS_ACCESS_KEY_IDはdummyであるべき")
+	}
+	if awsSecretAccessKey != "dummy" {
+		t.Fatal("環境変数AWS_SECRET_ACCESS_KEYはdummyであるべき")
 	}
 }
